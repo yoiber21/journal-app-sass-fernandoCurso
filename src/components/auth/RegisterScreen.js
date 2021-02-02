a8f43c067a94e370c97bedceaebe8cbe4361ea53
Yoiber <yoiber@yoiber.yoiber>
2021-02-02 18:33:20 -0500

    Configuracion del formulario de registro de usuarios

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/UseForm';
 
 export const RegisterScreen = () => {
+
+
+    const [ formValues, handleInputChange, /* reset */ ] = useForm({
+        name: 'Yoiber Beitar',
+        email: 'yoiber@gmail.com',
+        password: 123456,
+        password2: 123456
+    })
+    const { name, email, password, password2 }= formValues;
+    const handleRegister = e =>{
+        e.preventDefault();
+        // reset();
+    }
+
+    const isValidForm =()=>{
+        
+    }
+
     return (
         <>
             <h3 className="auth__title">Register</h3>
 
-            <form>
+            <form onSubmit={ (e)=>handleRegister(e) }>
 
                 <input 
                     type="text"
                     placeholder="Name"
                     name="name"
+                    value={ name }
                     className="auth__input"
+                    onChange={ (e)=>handleInputChange(e) }
                     autoComplete="off"
                 />
 
@@ -20,7 +41,9 @@ export const RegisterScreen = () => {
                     type="text"
                     placeholder="Email"
                     name="email"
+                    value={ email }
                     className="auth__input"
+                    onChange={ (e)=>handleInputChange(e) }
                     autoComplete="off"
                 />
 
@@ -28,14 +51,18 @@ export const RegisterScreen = () => {
                     type="password"
                     placeholder="Password"
                     name="password"
+                    value={ password }
                     className="auth__input"
+                    onChange={ (e)=>handleInputChange(e) }
                 />
 
                 <input 
                     type="password"
                     placeholder="Confirm password"
                     name="password2"
+                    value={ password2 }
                     className="auth__input"
+                    onChange={ (e)=>handleInputChange(e) }
                 />
 
 
