bddee916a37f25a1c32ea503e4db51a495623a79
Yoiber <yoiber@yoiber.yoiber>
2021-02-02 18:51:46 -0500

    Manejo de errores en el formulario de registro

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/UseForm';
+import validator from 'validator';
 
 export const RegisterScreen = () => {
 
@@ -12,13 +13,28 @@ export const RegisterScreen = () => {
         password2: 123456
     })
     const { name, email, password, password2 }= formValues;
+
+
     const handleRegister = e =>{
         e.preventDefault();
         // reset();
+        if ( isValidForm() ) console.log('Formulario correcto') 
     }
 
     const isValidForm =()=>{
+        if ( name.trim().length === 0 ) {
+            console.log('Name is required');
+            return false;
+        } else if( !validator.isEmail( email )){
+            console.log('Email is not valid');
+            return false;
+        }else if( password !== password2  || password.length <= 5 ){
+            console.log('password should be at least 6 characters and match each other');
+            return false;
+        }
+
         
+        return true
     }
 
     return (
@@ -26,7 +42,9 @@ export const RegisterScreen = () => {
             <h3 className="auth__title">Register</h3>
 
             <form onSubmit={ (e)=>handleRegister(e) }>
-
+                <div className="auth__alert-error">
+                    Error en el Formulario
+                </div>
                 <input 
                     type="text"
                     placeholder="Name"
